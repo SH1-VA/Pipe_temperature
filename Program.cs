1f9d1040008e9224cabdc80bec98b68065eedfd2
Shirokov Ivan <ivan-shirokov5@ya.ru>
2022-10-22 00:47:31 +0300

    Починил индексы

--- Program.cs
+++ Program.cs
@@ -23,36 +23,48 @@ internal class Program
             temperature[i, 60] = 100;
         }
         //Задаём температуру воды внутри
-        for(int i = 18; i < 42; i++)
+        for(int i = 18; i <= 42; i++)
         {
-            for(int j = 18; j < 42; j++)
+            for(int j = 18; j <= 42; j++)
             {
                 temperature[i, j] = 200;
             }
         }
 
-        //Теперь обсчитываем сечение
-        //for (int i = 1; i < 59; i++)
+        //Выводим значения для проверки
+        //for (int j = 60; j >= 0; j--)
         //{
-        //    if (i >= 18 && i <= 42)
+        //    for (int i = 0; i <= 60; i++)
         //    {
-        //        for (int j = 1; j < 17; j++)
-        //        {
-        //            temperature[i, j] = 1.0/4*(temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
-        //        }
-        //        for (int j = 43; j < 59; j++)
-        //        {
-        //            temperature[i, j] = 1.0 / 4 * (temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
-        //        }
+        //        var temp = string.Format("{0:f0}", temperature[i, j]);
+        //        Console.Write($"{temp,3}", temp);
         //    }
-        //    else
-        //    {
-        //        for (int j = 1; j < 59; j++)
-        //        {
-        //            temperature[i, j] = 1.0 / 4 * (temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
-        //        }
-        //    }
+        //    Console.WriteLine();
         //}
+        //Console.WriteLine();
+
+        //Теперь обсчитываем сечение
+        for (int i = 1; i <= 59; i++)
+        {
+            if (i >= 18 && i <= 42)
+            {
+                for (int j = 1; j <= 17; j++)
+                {
+                    temperature[i, j] = 1.0 / 4 * (temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
+                }
+                for (int j = 43; j < 59; j++)
+                {
+                    temperature[i, j] = 1.0 / 4 * (temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
+                }
+            }
+            else
+            {
+                for (int j = 1; j <= 59; j++)
+                {
+                    temperature[i, j] = 1.0 / 4 * (temperature[i - 1, j] + temperature[i, j - 1] + temperature[i + 1, j] + temperature[i, j + 1]);
+                }
+            }
+        }
 
         //Выводим значения
         for (int j = 60; j >= 0; j--)
